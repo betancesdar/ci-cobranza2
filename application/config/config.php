71c8912370625f72d8d7a9e3650a39925206f6c9
betancesdar <97894736+betancesdar@users.noreply.github.com>
2022-01-29 13:46:37 -0400

    third commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci-cobranza2/';
+$config['base_url'] = 'http://143.198.179.124//ci-cobranza2/';
 
 /*
 |--------------------------------------------------------------------------
