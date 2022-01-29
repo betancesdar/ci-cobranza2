5b825461b1e888f70eb0577b2cf30c94907dd810
betancesdar <97894736+betancesdar@users.noreply.github.com>
2022-01-29 13:40:17 -0400

    second commit

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'http://162.243.161.247/',
 	'username' => 'admin',
 	'password' => '$Joseyami0802',
 	'database' => 'prestoapp',
